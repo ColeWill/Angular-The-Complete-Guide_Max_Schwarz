1edbd5e3b7c7dda6c314890d97cc166ed4066b08
cowilliams_deloitte <cowilliams@deloitte.com>
2025-01-21 15:53:44 -0700

    51.

--- section02/tasks-list-app-max-schwarz/src/app/tasks/tasks.component.ts
+++ section02/tasks-list-app-max-schwarz/src/app/tasks/tasks.component.ts
@@ -1,18 +1,18 @@
 import { Component, Input } from "@angular/core";
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: "app-tasks",
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: "./tasks.component.html",
   styleUrl: "./tasks.component.css",
 })
 export class TasksComponent {
   @Input({ required: true }) name!: string;
   @Input({ required: true }) userId!: string;
-
-  usersTasks = [];
+  isAddingTask = false;
 
   tasks = [
     {
@@ -58,5 +58,9 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
