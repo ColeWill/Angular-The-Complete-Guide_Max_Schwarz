d2696e4583e66b75a4b61301e651406528aee9b9
cowilliams_deloitte <cowilliams@deloitte.com>
2025-01-21 16:44:56 -0700

    56.

--- section02/tasks-list-app-max-schwarz/src/app/tasks/new-task/new-task.component.ts
+++ section02/tasks-list-app-max-schwarz/src/app/tasks/new-task/new-task.component.ts
@@ -1,16 +1,30 @@
 import { Component, Output, EventEmitter } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { NewTaskData } from "../task.model";
 
 @Component({
   selector: "app-new-task",
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: "./new-task.component.html",
   styleUrl: "./new-task.component.css",
 })
 export class NewTaskComponent {
   @Output() cancelAdd = new EventEmitter<void>();
+  @Output() addTask = new EventEmitter<NewTaskData>();
+  enteredTitle = "";
+  enteredSummary = "";
+  enteredDueDate = "";
 
   onCancel() {
     this.cancelAdd.emit();
+  }
+
+  onSubmit() {
+    this.addTask.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDueDate,
+    });
   }
 }
