631c9d56268dd9b8094f906a82d23fc93b84de65
cowilliams_deloitte <cowilliams@deloitte.com>
2025-01-21 14:10:58 -0700

    renamed project folder

---
+++ section02/tasks-list-app-max-schwarz/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from "./user/users.data";
+import { CommonModule } from "@angular/common";
+import { TasksComponent } from "./tasks/tasks.component";
+
+export interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+@Component({
+  selector: "app-root",
+  standalone: true,
+  imports: [HeaderComponent, UserComponent, CommonModule, TasksComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId = "";
+  selectedUser: User = { id: "", name: "", avatar: "" };
+
+  onSelectUser(id: string) {
+    console.log("selected user id:", id);
+    if (id) {
+      this.selectedUserId = id;
+    }
+    this.selectedUser = DUMMY_USERS.find(user => user.id === id) || {
+      name: "",
+      id: "",
+      avatar: "",
+    };
+  }
+}
