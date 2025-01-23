322caf0ba7fcac44add15e3bb612d4bb6b56e44a
cowilliams_deloitte <cowilliams@deloitte.com>
2025-01-23 10:57:23 -0700

    finished practice app

--- 04-section-Angular-Practice/investment-calculator-section04/src/app/app.component.ts
+++ 04-section-Angular-Practice/investment-calculator-section04/src/app/app.component.ts
@@ -1,19 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
-
-import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    HeaderComponent,
-    UserInputComponent,
-    InvestmentResultsComponent,
-    CommonModule,
-  ],
-  standalone: true,
+
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
